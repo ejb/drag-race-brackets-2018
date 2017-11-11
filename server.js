ce725d994b9f0c6e599cc66e2565d8f6c573a23e
ejb <mail@elliotbentley.com>
2017-11-11 15:52:42 -0500

    Google Auth basic example

--- server.js
+++ server.js
@@ -1,80 +1,94 @@
-import express from 'express';
-import passport from 'passport';
-
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { match, RouterContext } from 'react-router';
-
-import { routes } from './app/routes';
-import configurePassport from './app/passport';
-
+const http = require('http');
+const express = require('express');
+const Session = require('express-session');
+const google = require('googleapis');
+const plus = google.plus('v1');
+ 
 const app = express();
+app.use(Session({
+    secret: 'raysources-secret-19890913007',
+    resave: true,
+    saveUninitialized: true
+}));
 
-app
-  .use(express.static('public'))
-  .set('view engine', 'ejs')
-
-
-configurePassport(passport);
-
-app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
-
-// handle the callback after facebook has authenticated the user
-app.get('/auth/facebook/callback',
-    passport.authenticate('facebook', {
-        successRedirect : '/profile',
-        failureRedirect : '/'
-    }));
-
-// route for logging out
-app.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
-});
-
-
-app.get('*', (req, res) => {
-  // routes is our object of React routes defined above
-  match({ routes, location: req.url }, (err, redirectLocation, props) => {
-    if (err) {
-      // something went badly wrong, so 500 with a message
-      res.status(500).send(err.message);
-    } else if (redirectLocation) {
-      // we matched a ReactRouter redirect, so redirect from the server
-      res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-    } else if (props) {
-      // if we got props, that means we found a valid component to render
-      // for the given route
-      const markup = renderToString(<RouterContext {...props} />);
-
-      // render `index.ejs`, but pass in the markup we want it to display
-      res.render('index', { markup })
-
-    } else {
-      // no route match, so 404. In a real app you might render a custom
-      // 404 view here
-      res.sendStatus(404);
-    }
-  });
-});
-
-
-// route middleware to make sure a user is logged in
-function isLoggedIn(req, res, next) {
-
-    // if user is authenticated in the session, carry on
-    if (req.isAuthenticated())
-        return next();
-
-    // if they aren't redirect them to the home page
-    res.redirect('/');
+const {GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET} = process.env;
+const RedirectionUrl = "http://localhost:1234/oauthCallback";
+function getOAuthClient () {
+    return new google.auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, RedirectionUrl);
 }
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, 'localhost', function(err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log('Listening at http://localhost:'+PORT);
+ 
+function getAuthUrl () {
+    const oauth2Client = getOAuthClient();
+    // generate a url that asks permissions for Google+ and Google Calendar scopes
+    const scopes = [
+      'https://www.googleapis.com/auth/plus.me'
+    ];
+ 
+    var url = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: scopes
+    });
+ 
+    return url;
+}
+ 
+app.use("/oauthCallback", function (req, res) {
+    const oauth2Client = getOAuthClient();
+    const session = req.session;
+    const code = req.query.code;
+    oauth2Client.getToken(code, function(err, tokens) {
+      // Now tokens contains an access_token and an optional refresh_token. Save them.
+      if(!err) {
+        oauth2Client.setCredentials(tokens);
+        session.tokens = tokens;
+        res.send(`
+            <h3>Login successful!!</h3>
+            <a href="/details">Go to details page</a>
+        `);
+      }
+      else{
+        res.send(`
+            <h3>Login failed!!</h3>
+        `);
+      }
+    });
 });
+ 
+app.use("/details", function (req, res) {
+    const oauth2Client = getOAuthClient();
+    oauth2Client.setCredentials(req.session["tokens"]);
+ 
+    const p = new Promise(function (resolve, reject) {
+        plus.people.get({ userId: 'me', auth: oauth2Client }, function(err, response) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(response);
+          }
+        });
+    })
+    .then(function (data) {
+        res.send(`
+            <img src=${data.image.url} />
+            <h3>Hello ${data.displayName}</h3>
+        `);
+    })
+    .catch(err => console.error(err));
+});
+ 
+app.use("/", function (req, res) {
+    var url = getAuthUrl();
+    res.send(`
+        <h1>Authentication using google oAuth</h1>
+        <a href=${url}>Login</a>
+    `)
+});
+ 
+ 
+const port = process.env.PORT || 1234;
+const server = http.createServer(app);
+server.listen(port);
+server.on('listening', function () {
+    console.log(`listening to ${port}`);
+});
+ 
